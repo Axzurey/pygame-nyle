0a19165094dbcb20dad84a8f3278e1c1b3cad8d0
Axzurey <66486198+Axzurey@users.noreply.github.com>
2022-10-05 12:35:41 -0400

    hello

--- classes/nominal.py
+++ classes/nominal.py
@@ -2,16 +2,39 @@ from typing import Any
 
 from pygame import Vector2
 import pygame
+from classes.color4 import color4
+from classes.udim2 import udim2
 
 class hoverable: ...
 
+def Corner(DX: float, DY: float, R: float):
+    return DX < 0 and DY < 0 and DX * DX + DY * DY > R * R
+
+
 class clickable:
     def isPointInBounding(self: Any, m: Vector2):
-        #https://math.stackexchange.com/questions/3630329/how-to-check-if-a-point-px-y-lies-inside-a-rounded-rectangle
+
+        from gui.guiObject import guiObject
+        from client.renderer import gameLoop
+        
         if self.boundingRect:
-            r = self.cornerRadius or 0
             br: pygame.Rect = self.boundingRect
+            r = self.cornerRadius or 0
+            if r < 60:
+                Top = br.y
+                Left = br.x
+                X = m.x
+                Y = m.y
+                (Wdt, Hgt) = br.size
+                R = r
+                for e in ((Left, Top), (X, Y)):
+                    x = guiObject()
+                    x.size = udim2.fromOffset(5, 5)
+                    x.backgroundColor = color4(1, .5, 1, 1)
+                    x.parent = gameLoop.getRenderer()
+                    x.position = udim2.fromOffset(e[0], e[1])
             
+            """
             (a, b, c, d) = (Vector2(br.topleft), Vector2(br.topright), Vector2(br.bottomright), Vector2(br.bottomleft))
 
             (x1, y1) = (a.x, a.y)
@@ -24,19 +47,17 @@ class clickable:
             D2 = (x3 - x2) * (yp - y2) - (xp - x2) * (y3 - y2)
             D3 = (x4 - x3) * (yp - y3) - (xp - x3) * (y4 - y3)
             D4 = (x1 - x4) * (yp - y4) - (xp - x4) * (y1 - y4)
+            """
+            Top = br.y
+            Left = br.x
+            X = m.x
+            Y = m.y
+            (Wdt, Hgt) = br.size
+            R = r
 
-            if D1 > 0 and D2 > 0 and D3 > 0 and D4 > 0:
-                sq1c1 = pos + size - Vector2(r, r)
-                sq1c2 = pos + size
+            Outside = (X < Left) or (X > Left + Wdt) or (Y < Top) or (Y > Top + Hgt) or Corner(X - Left - R - X, Y - Top - R, R) or Corner(X - Left - R, Top  + Hgt - R - Y, R) or Corner(Left + Wdt - R - X, Top  + Hgt - R - Y, R) or Corner(Left + Wdt - R - X, Y - Top - R, R)
 
-                sq2c1 = pos
-                sq2c2 = pos + Vector2(r, r)
-
-                sq3c1 = pos + Vector2(0, size.y)
-                sq3c2 = pos + Vector2(0, size.y) + Vector2(r, -r)
-
-                sq4c1 = pos + Vector2(size.x, 0)
-                sq4c2 = pos + Vector2(size.x, 0) + Vector2(-r, r)
-                
-            else:
-                return False
+            if Outside: return False
+            return True
+        else:
+            return False
